3be1f25329cadfc3502fa807dde1f994a5603a7b
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-09-08 13:43:55 +0200

    Update: clean personal.sh

--- overlay/etc/cont-init.d/999-personal.sh
+++ overlay/etc/cont-init.d/999-personal.sh
@@ -1,8 +1,2 @@
 echo "**** Fix mtu issues ****"
-	ifconfig enp6s0 mtu 1500
-
-echo "*** Changing default shell ***"
-	pacman -Syu --noconfirm \
-	zsh
-
-	chsh -s /bin/bash
+   ifconfig enp7s0 mtu 1500
